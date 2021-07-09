a1a3e9490462338ec5422928a5e85828f078ad5f
Lemon4ek <86946311+Lemon4ek@users.noreply.github.com>
2021-07-09 23:20:06 +0300

    Update task1.py

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,1 +1,36 @@
-task1 
+import re
+
+rad=input("\nРядок: ")
+
+print("\nРядок: ", rad)
+
+el="".join(re.split("[0-9]+", rad))
+en="".join(re.split("[a-zA-Z]+", rad))
+
+
+elk=", ".join(el.split())
+enk=", ".join(en.split())
+
+
+print("\nЧисла: ", str(enk))
+
+print("\nРяд без чисел: ", str(elk))
+
+
+row_new_list=[]
+for let in el.split():
+    row_new_list.append(let[0].upper() + let[1:-1].lower() + let[-1].upper())
+upper_let=" ".join(row_new_list)
+print("\nВелика літера: ", upper_let)
+
+
+new_num_max=list(map(int, en.split()))
+
+
+max_num=max(new_num_max)
+
+
+print("\nМаксимальне значення: ", max_num)
+
+
+
